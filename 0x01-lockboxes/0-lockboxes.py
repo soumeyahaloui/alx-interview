1e14850c7695ffcdbdbf48885d1453f129fa7168
soumeyahaloui <soumeyahaloui@gmail.com>
2024-01-11 16:09:07 -0800

    updated

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -22,3 +22,18 @@ def canUnlockAll(boxes):
     return len(unlocked) == len(boxes)
 
 
+if __name__ == "__main__":
+    # Test case 1: Sequential unlocking
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))  # Expected output: True
+
+    # Test case 2: Keys that don't have corresponding boxes
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))  # Expected output: True
+
+    # Test case 3: Not all boxes can be unlocked
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))  # Expected output: False
+
+    # Additional test cases can be added here
+
