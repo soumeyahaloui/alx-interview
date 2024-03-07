57dd4cfdd362bef1d469bf4494b9328b3f2533b0
soumeyahaloui <soumeyahaloui@gmail.com>
2024-03-07 13:02:55 -0800

    updated

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+0-island_perimeter
+"""
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island described in the grid.
+
+    Args:
+        grid (List[List[int]]): A list of lists representing the island.
+
+    Returns:
+        int: The perimeter of the island.
+    """
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 4  # Initial perimeter for land cell
+
+                # Check neighboring cells, subtract 2 for each adjacent land cell
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
+
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
